506907ba7ef2c41fd780fd58123b265ef6137beb
Jacky Zhao <zhao.jackyy@gmail.com>
2020-04-05 00:40:48 -0400

    adjust deploy setting

--- index.js
+++ index.js
@@ -15,37 +15,50 @@ app.use(cors());
 app.use(router);
 
 io.on('connect', (socket) => {
-  socket.on('join', ({ name, room }, callback) => {
-    const { error, user } = addUser({ id: socket.id, name, room });
+    socket.on('join', ({ name, room }, callback) => {
+        const { error, user } = addUser({ id: socket.id, name, room });
 
-    if(error) return callback(error);
+        if (error) return callback(error);
 
-    socket.join(user.room);
+        socket.join(user.room);
 
-    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
-    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.` });
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
 
-    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
 
-    callback();
-  });
+        callback();
+    });
 
-  socket.on('sendMessage', (message, callback) => {
-    const user = getUser(socket.id);
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
 
-    io.to(user.room).emit('message', { user: user.name, text: message });
+        io.to(user.room).emit('message', { user: user.name, text: message });
 
-    callback();
-  });
+        callback();
+    });
 
-  socket.on('disconnect', () => {
-    const user = removeUser(socket.id);
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id);
 
-    if(user) {
-      io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
-      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
-    }
-  })
+        if (user) {
+            io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+            io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+        }
+    })
 });
 
-server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+// server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
